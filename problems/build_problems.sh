0b1ff25958fc9e14ed1e401a117322f99aedd79a
Dan Seminara <semi225599@gmail.com>
2021-10-10 01:04:51 -0400

    Simplify generation

--- problems/build_problems.sh
+++ problems/build_problems.sh
@@ -2,32 +2,36 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-echo "package problems" > "$SCRIPT_DIR/problems.go"
-echo "" >> "$SCRIPT_DIR/problems.go"
-echo "import (" >> "$SCRIPT_DIR/problems.go"
-for d in *; do
-  if [[ $d == year* ]]; then
-    echo -e "\t\"advent/$d\"" >> "$SCRIPT_DIR/problems.go"
-  fi
-done
-echo ")" >> "$SCRIPT_DIR/problems.go"
-echo "" >> "$SCRIPT_DIR/problems.go"
-echo "type Day struct {" >> "$SCRIPT_DIR/problems.go"
-echo -e "\tPart1 func(string) interface{}" >> "$SCRIPT_DIR/problems.go"
-echo -e "\tPart2 func(string) interface{}" >> "$SCRIPT_DIR/problems.go"
-echo "}" >> "$SCRIPT_DIR/problems.go"
-echo "" >> "$SCRIPT_DIR/problems.go"
-echo "var Probs = map[int]map[int]Day{" >> "$SCRIPT_DIR/problems.go"
-for d in *; do
-  if [[ $d == year* ]]; then
-    echo -e "\t${d:4}: map[int]Day{" >> "$SCRIPT_DIR/problems.go"
-    for i in {01..25}; do
-      if [[ -f "$d/day$i.go" ]]; then
-        n=$(echo $i | sed 's/^0*//')
-        echo -e "\t\t$n: Day{$d.Day${i}Part1, $d.Day${i}Part2}," >> "$SCRIPT_DIR/problems.go"
-      fi
-    done
-    echo -e "\t}," >> "$SCRIPT_DIR/problems.go"
-  fi
-done
-echo "}" >> "$SCRIPT_DIR/problems.go"
+function output_problems {
+  echo "package problems"
+  echo ""
+  echo "import ("
+  for d in *; do
+    if [[ $d == year* ]]; then
+      echo -e "\t\"advent/$d\""
+    fi
+  done
+  echo ")"
+  echo ""
+  echo "type Day struct {"
+  echo -e "\tPart1 func(string) interface{}"
+  echo -e "\tPart2 func(string) interface{}"
+  echo "}"
+  echo ""
+  echo "var Probs = map[int]map[int]Day{"
+  for d in *; do
+    if [[ $d == year* ]]; then
+      echo -e "\t${d:4}: map[int]Day{"
+      for i in {01..25}; do
+        if [[ -f "$d/day$i.go" ]]; then
+          n=$(echo $i | sed 's/^0*//')
+          echo -e "\t\t$n: Day{$d.Day${i}Part1, $d.Day${i}Part2},"
+        fi
+      done
+      echo -e "\t},"
+    fi
+  done
+  echo "}"
+}
+
+output_problems > "$SCRIPT_DIR/problems.go"
