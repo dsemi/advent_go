c7c5e0916e51d68500b76b216aef096e2b2dd429
Dan Seminara <semi225599@gmail.com>
2021-10-10 02:44:17 -0400

    Add network fetch

--- advent.go
+++ advent.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"advent/problems"
+	"bytes"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -15,11 +19,46 @@ import (
 
 //go:generate ./problems/build_problems.sh
 
-func getInput(year, day int) string {
-	// TODO network
+const rateLimit = 5 * time.Second
+var last = new(time.Time)
+
+func getInput(year, day int, download bool) string {
 	_, b, _, _ := runtime.Caller(0)
 	inputFile := filepath.Join(filepath.Dir(b), fmt.Sprintf("inputs/%d/input%d.txt", year, day))
-	buf, _ := ioutil.ReadFile(inputFile)
+	buf, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) && download {
+			fmt.Printf("Downloading input for Year %d Day %d\n", year, day)
+			if last != nil {
+				time.Sleep(time.Until(last.Add(rateLimit)))
+			}
+			*last = time.Now()
+			url := fmt.Sprintf("https://adventofcode.com/%d/day/%d/input", year, day)
+			client := &http.Client{}
+			req, _ := http.NewRequest("GET", url, nil)
+			req.Header.Set("Cookie", os.Getenv("AOC_SESSION"))
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Fatalf("Problem input fetch failed: %v", err)
+			}
+			if resp.StatusCode < 200 || resp.StatusCode > 299 {
+				log.Fatalf("Bad HTTP response: %v", resp)
+			}
+			b := new(bytes.Buffer)
+			if 	_, err = b.ReadFrom(resp.Body); err != nil {
+				log.Fatalf("Error reading HTTP response body: %v", err)
+			}
+			if err = resp.Body.Close(); err != nil {
+				log.Fatalf("Error closing HTTP response body: %v", err)
+			}
+			buf = b.Bytes()
+			if err = ioutil.WriteFile(inputFile, buf, 0644); err != nil {
+				log.Fatalf("Unable to write to output file: %v", err)
+			}
+		} else {
+			log.Fatalf("Error reading problem input file: %v", err)
+		}
+	}
 	return strings.TrimRightFunc(string(buf), unicode.IsSpace)
 }
 
@@ -65,7 +104,7 @@ func main() {
 	var total float64
 	for _, day := range days {
 		day, _ := strconv.Atoi(day)
-		input := getInput(year, day)
+		input := getInput(year, day, true)
 		total += runProblem(year, day, input)
 	}
 	fmt.Printf("Total: %53.3f seconds\n", total)
