d1117c2d42b6b8d4dc85f822434b667d7679212c
Dan Seminara <semi225599@gmail.com>
2022-06-20 13:36:53 -0400

    2021 Day 4

---
+++ year2021/day04.go
@@ -0,0 +1,92 @@
+package year2021
+
+import (
+	"advent/problems"
+	"advent/utils"
+	"strings"
+)
+
+type Day04 struct{}
+
+type board struct {
+	grid [][]int
+	done bool
+}
+
+func (b *board) isWinner() bool {
+outer:
+	for i := 0; i < len(b.grid); i++ {
+		var r, c bool
+		for j := 0; j < len(b.grid); j++ {
+			r = r || b.grid[i][j] != -1
+			c = c || b.grid[j][i] != -1
+			if r && c {
+				continue outer
+			}
+		}
+		return true
+	}
+	return false
+}
+
+func (d *Day04) winnerScores(input string) chan int {
+	c := make(chan int)
+	go func() {
+		v := strings.Split(input, "\n\n")
+		nums := v[0]
+		var boards []*board
+		for _, brd := range v[1:] {
+			b := &board{}
+			b.grid = make([][]int, 0)
+			for r, line := range strings.Split(brd, "\n") {
+				b.grid = append(b.grid, make([]int, 0))
+				for _, v := range strings.Fields(line) {
+					b.grid[r] = append(b.grid[r], utils.Int(v))
+				}
+			}
+			boards = append(boards, b)
+		}
+		for _, num := range strings.Split(nums, ",") {
+			n := utils.Int(num)
+			for _, b := range boards {
+				if b.done {
+					continue
+				}
+				for i, row := range b.grid {
+					for j, v := range row {
+						if v == n {
+							b.grid[i][j] = -1
+						}
+					}
+				}
+				if b.isWinner() {
+					b.done = true
+					var sum int
+					for _, row := range b.grid {
+						for _, v := range row {
+							if v != -1 {
+								sum += v
+							}
+						}
+					}
+					sum *= n
+					c <- sum
+				}
+			}
+		}
+		close(c)
+	}()
+	return c
+}
+
+func (d *Day04) Part1(input string) interface{} {
+	return <-d.winnerScores(input)
+}
+
+func (d *Day04) Part2(input string) interface{} {
+	return utils.Last(d.winnerScores(input))
+}
+
+func init() {
+	problems.Register(&Day04{})
+}
