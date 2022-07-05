9abe346c4bb5beae3c848001bee282cfc2ef520b
Dan Seminara <semi225599@gmail.com>
2022-07-04 22:00:50 -0400

    Shorten 2015 Day 7

--- year2015/day07.go
+++ year2015/day07.go
@@ -4,20 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
-	"sync"
 )
-
-func lazy(f func() uint16) func() uint16 {
-	var ans uint16
-	var once sync.Once
-	return func() uint16 {
-		once.Do(func() {
-			ans = f()
-			f = nil
-		})
-		return ans
-	}
-}
 
 func Part1(input string) interface{} {
 	m := make(map[string]func() uint16)
@@ -27,32 +14,28 @@ func Part1(input string) interface{} {
 		}
 		return m[x]()
 	}
+	ops := map[string]func(a, b uint16) uint16{
+		"->":     func(a, b uint16) uint16 { return a },
+		"NOT":    func(a, b uint16) uint16 { return ^a },
+		"AND":    func(a, b uint16) uint16 { return a & b },
+		"OR":     func(a, b uint16) uint16 { return a | b },
+		"LSHIFT": func(a, b uint16) uint16 { return a << b },
+		"RSHIFT": func(a, b uint16) uint16 { return a >> b },
+	}
 	for _, line := range strings.Split(input, "\n") {
 		pts := strings.Fields(line)
-		if len(pts) == 3 {
-			m[pts[2]] = lazy(func() uint16 {
-				return val(pts[0])
-			})
-		} else if len(pts) == 4 {
-			m[pts[3]] = lazy(func() uint16 {
-				return ^val(pts[1])
-			})
-		} else if pts[1] == "AND" {
-			m[pts[4]] = lazy(func() uint16 {
-				return val(pts[0]) & val(pts[2])
-			})
-		} else if pts[1] == "OR" {
-			m[pts[4]] = lazy(func() uint16 {
-				return val(pts[0]) | val(pts[2])
-			})
-		} else if pts[1] == "LSHIFT" {
-			m[pts[4]] = lazy(func() uint16 {
-				return val(pts[0]) << val(pts[2])
-			})
-		} else if pts[1] == "RSHIFT" {
-			m[pts[4]] = lazy(func() uint16 {
-				return val(pts[0]) >> val(pts[2])
-			})
+		if len(pts) < 5 {
+			m[pts[len(pts)-1]] = func() uint16 {
+				ans := ops[pts[4-len(pts)]](val(pts[len(pts)-3]), 0)
+				m[pts[len(pts)-1]] = func() uint16 { return ans }
+				return ans
+			}
+		} else {
+			m[pts[len(pts)-1]] = func() uint16 {
+				ans := ops[pts[1]](val(pts[0]), val(pts[2]))
+				m[pts[len(pts)-1]] = func() uint16 { return ans }
+				return ans
+			}
 		}
 	}
 	return val("a")
