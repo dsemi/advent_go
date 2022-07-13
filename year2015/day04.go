f4bfb105a68bd1ebee1000a4262408e6ce1a6aed
Dan Seminara <semi225599@gmail.com>
2022-07-12 22:27:57 -0400

    Simplify

--- year2015/day04.go
+++ year2015/day04.go
@@ -5,22 +5,19 @@ import (
 	"strconv"
 )
 
-func sum(s string, i int) [16]byte {
-	return md5.Sum([]byte(s + strconv.Itoa(i)))
+func solve(input string, n byte) int {
+	for i := 0; ; i++ {
+		hash := md5.Sum([]byte(input + strconv.Itoa(i)))
+		if hash[0] == 0 && hash[1] == 0 && hash[2] <= n {
+			return i
+		}
+	}
 }
 
 func Part1(input string) interface{} {
-	i := 0
-	for hash := sum(input, i); hash[0] != 0 || hash[1] != 0 || hash[2] > 15; hash = sum(input, i) {
-		i++
-	}
-	return i
+	return solve(input, 15)
 }
 
 func Part2(input string) interface{} {
-	i := 0
-	for hash := sum(input, i); hash[0] != 0 || hash[1] != 0 || hash[2] != 0; hash = sum(input, i) {
-		i++
-	}
-	return i
+	return solve(input, 0)
 }
